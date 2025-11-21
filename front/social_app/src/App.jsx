2921572ef759c10b82dd73c9209f465a3a565124
Sofiya Vasileva <sv6.4@mail.ru>
2025-11-22 01:40:49 +0400

    friends

--- front/social_app/src/App.jsx
+++ front/social_app/src/App.jsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Feed from './pages/feed/Feed'
 import Messages from './pages/message/Messages'
-<<<<<<< HEAD
-import Profile from "./pages/profile/Profile";
-=======
 import Profile from './pages/profile/Profile'
 import Auth from './pages/auth/Auth'
 import NotFound from './pages/notfound/NotFound'
 import Communities from './pages/communities/Communities'
-
->>>>>>> 25c35090b70e730d3b0751acdd92051a81bbaac5
+import Friends from './pages/friends/Friends'
 
 function App() {
   return (
@@ -18,13 +14,11 @@ function App() {
         <Route path="/" element={<Feed />} />
         <Route path="/messages" element={<Messages />} />
         <Route path="/profile" element={<Profile />} />
-<<<<<<< HEAD
-=======
         <Route path="/auth" element={<Auth />} />
         <Route path="/communities" element={<Communities />} />
-        
+        <Route path='/friends' element={<Friends/>}/>
+
         <Route path="*" element={<NotFound />} />
->>>>>>> 25c35090b70e730d3b0751acdd92051a81bbaac5
       </Routes>
     </BrowserRouter>
   )
