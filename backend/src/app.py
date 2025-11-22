db217aed3816aa5c725c4a2dda5184dfc35f0259
morshu <asdgrghd@gmail.com>
2025-11-22 10:27:29 +0400

    feat: updated CORS policy

--- backend/src/app.py
+++ backend/src/app.py
@@ -8,10 +8,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"], 
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 include_routers(app)
