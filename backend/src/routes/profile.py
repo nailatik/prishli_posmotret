2e8fa943fb2d61cbc836a4d4dc9f4a9da82980f3
Alexxkowal <alexxkowal@mail.ru>
2025-11-22 12:21:18 +0400

    added recomendation logic

--- backend/src/routes/profile.py
+++ backend/src/routes/profile.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from ..database.db import get_db, get_current_user, get_user_data_by_id, get_posts_by_user_id
+from ..services.user_recommendations.smart_recommendation_service import SmartRecommendationService
 
 router = APIRouter()
 
@@ -18,6 +19,19 @@ async def get_profile(
     posts = await get_posts_by_user_id(session, user_id)
 
     is_own_profile = user.user_id == current_user.user_id
+    recommender = SmartRecommendationService(session)
+    recommended_ids = await recommender.get_top(user_id, top_n=10)
+    recommended_users = []
+
+    for rec_id in recommended_ids:
+        rec_user = await get_user_data_by_id(session, rec_id)
+        if rec_user:
+            recommended_users.append({
+                "id": rec_user.user_id,
+                "first_name": rec_user.first_name,
+                "last_name": rec_user.last_name,
+                "avatar": rec_user.avatar_url
+            })
 
     return {
         "id": user.user_id,
@@ -27,4 +41,5 @@ async def get_profile(
         "bio": user.bio,
         "is_own_profile": is_own_profile,   # фронт может использовать для разных представлений
         "posts": [post.to_pydantic() for post in posts],
+        "recommended_friends": recommended_users
     }
